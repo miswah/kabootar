8f60b739aa522e0ac2830749e4cbb1787e6d71c9
miswah <miswah.md@gmail.com>
2026-08-17 17:57:14 +0530

    (init): add common module init spring

---
+++ common/src/test/java/io/kabootar/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kabootar.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
