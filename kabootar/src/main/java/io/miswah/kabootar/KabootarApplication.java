da5ae00123b01527b1680dbd0296efec277754fa
miswah <miswah.md@gmail.com>
2026-08-17 17:26:44 +0530

    (init): add base init spring project

---
+++ kabootar/src/main/java/io/miswah/kabootar/KabootarApplication.java
@@ -0,0 +1,13 @@
+package io.miswah.kabootar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabootarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabootarApplication.class, args);
+	}
+
+}
---
+++ kabootar/src/test/java/io/miswah/kabootar/KabootarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miswah.kabootar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabootarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
