f7b73f09b9c2456318c712cc1739950a60734674
miswah <miswah.md@gmail.com>
2026-08-31 16:22:43 +0530

    (enh): validation for errorPercentage in requestBody as record

--- simulator/src/main/java/io/kabootar/simulator/dto/request/ConfigRequestDTO.java
+++ simulator/src/main/java/io/kabootar/simulator/dto/request/ConfigRequestDTO.java
@@ -1,20 +1,6 @@
 package io.kabootar.simulator.dto.request;
 
-import lombok.Builder;
-import lombok.Data;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-@Data
-@Builder
-public class ConfigRequestDTO{
-    private Integer fixedDelaysMs;
-    private Integer jitterMs;
-    private Integer errorStatus;
-    private Integer maximumConcurrency;
-
-    @Max(value = 100)
-    @Min(value = 0)
-    private Double errorPercentage;
-    private boolean outage;
-}
+public record ConfigRequestDTO(Integer fixedDelaysMs, Integer jitterMs, Integer errorStatus, Integer maximumConcurrency, @Max(value= 100) @Min(value = 0) Double errorPercentage, boolean outage){}
