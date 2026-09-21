0f7102f859fa595ec68640e51edc2daafbee9f13
miswah <miswah.md@gmail.com>
2026-09-21 15:23:07 +0530

    (enh): add redis support to configuration service #15

---
+++ configuration/src/main/java/io/kabootar/configuration/utilities/RedisConfig.java
@@ -0,0 +1,27 @@
+package io.kabootar.configuration.utilities;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public <K,V> RedisTemplate<K, V> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<K, V> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
